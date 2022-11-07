4b4585fcdca939c1971313de3c4fdaebe52958f3
brunobat <brunobat@gmail.com>
2022-11-07 17:14:48 +0000

    graal-aws-rest

---
+++ graalvm-aws-rest-26250/src/test/java/com/brunobat/GreetingIT.java
@@ -0,0 +1,7 @@
+package com.brunobat;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingIT extends GreetingTest {
+}
---
+++ graalvm-aws-rest-26250/src/test/java/com/brunobat/GreetingTest.java
@@ -0,0 +1,18 @@
+package com.brunobat;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+
+@QuarkusTest
+public class GreetingTest
+{
+    @Test
+    public void testJaxrs() {
+        RestAssured.when().get("/hello").then()
+                .contentType("text/plain")
+                .body(equalTo("hello"));
+    }
+}
